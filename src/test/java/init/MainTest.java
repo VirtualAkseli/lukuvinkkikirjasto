3fb9cd5ff693720bdd5d258f11d18c82b22fea09
Akira Taguchi <akirahattivatti@gmail.com>
2020-11-20 16:58:15 +0200

    added mock test to trigger CI

---
+++ src/test/java/init/MainTest.java
@@ -0,0 +1,13 @@
+package init;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void mockToStartGithubActions() {
+        assertEquals(true, 1 == 1);
+    }
+}
