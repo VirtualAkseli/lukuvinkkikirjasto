0c9f44b58ed030e8fd15fa60fad3cd0846ab7e6d
ric-rai <48387692+Ric-Rai@users.noreply.github.com>
2020-11-26 17:09:26 +0200

    Make chart real-time

--- burndown/Koodi.js
+++ burndown/Koodi.js
@@ -1,11 +1,32 @@
+var ss = SpreadsheetApp.getActiveSpreadsheet(),
+    sheet = ss.getSheetByName("Data");
+
 /**
  * Scheduled main function
  */
 function fetchData() {
-  var response = UrlFetchApp.fetch(getURL());
-  var json = JSON.parse(response.getContentText());
-  var pattern = /\>([^<>]+)\((\s*\d+\s*\/\s*\d+\s*|\s*\d+\s*)\)\s*\</gm;
-  write(matchAndMap(json, pattern));
+  var projectUpdatedAt = fetchUpdatedAt();
+  var sheetUpdatedAt = sheet.getRange(1, 2).getValue();
+  if (sheetUpdatedAt !== projectUpdatedAt ) {
+    Logger.log("updating...");
+    sheet.getRange(1, 2).setValue(projectUpdatedAt);
+    var response = UrlFetchApp.fetch(getURL());
+    var json = JSON.parse(response.getContentText()),
+        pattern = /\>([^<>]+)\((\s*\d+\s*\/\s*\d+\s*|\s*\d+\s*)\)\s*\</gm;
+    write(matchAndMap(json, pattern));
+  }
+}
+
+function fetchUpdatedAt() {
+  var projectUrl = "https://api.github.com/projects/5974216";
+  var options = {
+    headers: {
+      Accept: "application/vnd.github.inertia-preview+json",
+      Authorization: "token " + PropertiesService.getScriptProperties().getProperty("TOKEN")
+    }
+  };
+  var json = JSON.parse(UrlFetchApp.fetch(projectUrl, options));
+  return json.updated_at;
 }
 
 /**
@@ -15,7 +36,7 @@ function fetchData() {
 function getURL() {
   var requestJson = {
     // URL of the current sprint board
-    url:"https://github.com/akirataguchi115/lukuvinkkikirjasto/projects/3",
+    url:"https://github.com/akirataguchi115/lukuvinkkikirjasto/projects/4",
     renderType:"plainText",
     outputAsJson:true
   };
@@ -39,9 +60,21 @@ function matchAndMap(json, pattern) {
 }
 
 function write(rowArray) {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.getSheetByName("Data");
-  for (var i = 0; i < rowArray.length; i++) {
+  var date = rowArray[0][0].substr(0, 10);
+  removePreviousEntries(date, sheet);
+  for (var i = 0; i < rowArray.length; i++)
     sheet.appendRow(rowArray[i]);
+}
+
+function removePreviousEntries(date) {
+  var lastRow = sheet.getLastRow();
+  var savedData = sheet.getRange(2, 1, lastRow - 1).getValues(),
+      count = 0;
+  for (var i = savedData.length - 1; i > 0; i--) {
+    if (savedData[i][0].toLocaleString("fi-FI").substr(0, 10) === date)
+      count++;
+    else break;
   }
+  for (i = lastRow; i > lastRow - count; i--)
+    sheet.deleteRow(i);
 }
